6dabe297fa7a62f514a50e6c2ee6843b0213bbc9
elliottmatt <elliottmatt@gmail.com>
2015-01-20 22:57:36 -0600

    elliottmatt cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matt Elliott (elliottmatt)
+# 2015/01/20
+# https://class.coursera.org/rprog-010/
+# R Programming
+# by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
 
-## Write a short comment describing this function
+# inversing a matrix can be an expensive operation
+# use these functions to create an object that caches the inverse
 
+# example usage:
+# > c <- rbind(c(1, -1/4), c(-1/4, 1))
+# > m <- makeCacheMatrix(c)
+# > cacheSolve(m)
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+# > cacheSolve(m)
+# getting cached data
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+
+
+## Input:  square matrix (assumes it is inverse-able)
+## Output: m object with 4 functions
+##         m is not useful alone and should be used with cacheSolve()
+##
+## This function sets up the matrix in a wrapper that will
+## cache the result once it is calculated once.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Input:  x must be the output of the 'makeCacheMatrix'
+## Output: matrix that is the inverse of the input
+##
+## This function will calculate the inverse matrix and store it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
